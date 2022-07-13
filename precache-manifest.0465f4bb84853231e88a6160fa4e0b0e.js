586eb941c5d6145d1c458e84e3c24bd00da1968b
poojaMourya <poojamourya@newrise.in>
2022-07-13 12:53:46 +0530

    Updates

---
+++ precache-manifest.0465f4bb84853231e88a6160fa4e0b0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1bdff38ffb8f8d19d33",
+    "url": "/uppercaseapp/static/css/main.e0c74763.chunk.css"
+  },
+  {
+    "revision": "f1bdff38ffb8f8d19d33",
+    "url": "/uppercaseapp/static/js/main.f1bdff38.chunk.js"
+  },
+  {
+    "revision": "97bc1625e33b04eb7828",
+    "url": "/uppercaseapp/static/js/1.97bc1625.chunk.js"
+  },
+  {
+    "revision": "f7c49957740a91072126",
+    "url": "/uppercaseapp/static/js/2.f7c49957.chunk.js"
+  },
+  {
+    "revision": "59ef4445899daafae138",
+    "url": "/uppercaseapp/static/js/runtime~main.59ef4445.js"
+  },
+  {
+    "revision": "0b4ba8a97eff13d601246986a961c4c6",
+    "url": "/uppercaseapp/index.html"
+  }
+];
